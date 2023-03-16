32163fa837128db4f169630b54ad52e550869c57
eclavel <113940257+eclavel@users.noreply.github.com>
2023-03-15 22:14:58 -0700

    javascript file works properly

--- main.js
+++ main.js
@@ -1,1 +1,52 @@
+const buttons = document.querySelectorAll('button');
+const display = document.querySelector('.display');
+const numbers = document.querySelectorAll('.number');
+const operators = document.querySelectorAll('.operator');
+const clear = document.querySelector('.clear');
+const equal = document.querySelector('#equal');
+
+numbers.forEach(number => {
+  number.addEventListener('click',e =>{
+    display.innerHTML += number.innerHTML;
+  });
+});
+
+operators.forEach(operator => {
+  operator.addEventListener('click', () =>{
+    console.log(operator.innerHTML);
+  });
+});
+
+function operate(operator, num1, num2){
+  if(operator === '+'){
+    console.log(add(num1,num2));
+  }
+  else if(operator === '-'){
+    console.log(subtract(num1,num2));
+  }
+  else if(operator === 'x'){
+    console.log(multiply(num1,num2));
+  }
+  else if(operator === '/'){
+    console.log(divide(num1,num2));
+  }
+}
+
+function add(num1, num2){
+  return num1 + num2;
+}
+
+function subtract(num1,num2){
+  return num1 - num2;
+}
+
+function multiply(num1, num2){
+  return num1*num2;
+}
+
+function divide(num1, num2){
+  return num1 / num2;
+}
+
+
 
